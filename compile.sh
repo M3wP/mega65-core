980fb1056966a5942cb3b23b1ac7cfa3272eb82f
Ben-401 <gerb0002@flinders.edu.au>
2017-04-11 10:44:35 +0930

    in part, updated project-files to reflect new locations, updated compile-script

--- compile.sh
+++ compile.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
 
-# first things first, make the ISE-project-script-file Read-Only for all groups.
-# this is done to stop the ISE-GUI from overwriting the file, which may be
-# causing the compile-script to fail due to invalid relative addresses.
-# a possible fix is to move the mega65.gise (ISE project file) into ./isework
-# BG: these commands are likely to be removed once I get around to implementing my 'cleaner' branch
-ls -al    isework/container.xst
-chmod a-w isework/container.xst
-ls -al    isework/container.xst
+# find out where we are, we should be in the toplevel mega65-core dir
+BASEDIR=$(pwd)
+# ise compile-script output dir
+SCROUTDIR="megascript"
 
-# ensure these directory exists, if not, make them
+
+# ensure these directories exists, if not, make them
 LOGDIR="build-logs"
-if test ! -e    "./${LOGDIR}"; then
-  echo "Creating ./${LOGDIR}"
-  mkdir          ./${LOGDIR}
+if test ! -e    "${BASEDIR}/${LOGDIR}"; then
+  echo "Creating ${BASEDIR}/${LOGDIR}"
+  mkdir          ${BASEDIR}/${LOGDIR}
 fi
-if test ! -e    "./sdcard-files"; then
-  echo "Creating ./sdcard-files"
-  mkdir          ./sdcard-files
+if test ! -e    "${BASEDIR}/sdcard-files"; then
+  echo "Creating ${BASEDIR}/sdcard-files"
+  mkdir          ${BASEDIR}/sdcard-files
 fi
-if test ! -e    "./sdcard-files/old-bitfiles"; then
-  echo "Creating ./sdcard-files/old-bitfiles"
-  mkdir          ./sdcard-files/old-bitfiles
+if test ! -e    "${BASEDIR}/sdcard-files/old-bitfiles"; then
+  echo "Creating ${BASEDIR}/sdcard-files/old-bitfiles"
+  mkdir          ${BASEDIR}/sdcard-files/old-bitfiles
 fi
 
+
+TARGET="nonddr" # filename to indicate target is Nexys4 (non-ddr) board
+# if the file exists, target the non-ddr board
+if test -e ${BASEDIR}/${TARGET}; then
+  echo "Compiling for the Nexys4 (non-DDR) board."
+  UCF_TARGET_FPGA="../../../src/vhdl/container-nonddr.ucf"
+  TARGET_TAG="${TARGET}"
+else
+  echo "Compiling for the Nexys4DDR board (the default)."
+  echo "To compile for the Nexys4 (non-DDR) board, the \"./${TARGET}\" file must exist"
+  UCF_TARGET_FPGA="../../../src/vhdl/container-ddr.ucf"
+  TARGET_TAG="ddr"
+fi
+
+
+# pre-compile
 ( cd src ; make generated_vhdl firmware ../doc/iomap.md tools utilities roms)
 retcode=$?
-
-if [ $retcode -ne 0 ] ; then
+#
+if test ! $retcode = 0; then
   echo "make failed with return code $retcode" && exit 1
 else
   echo "make completed."
   echo " "
 fi
 
+
 # here we need to detect if you have 64 or 32 bit machine
 # on a 64-bit installation, both 32 and 64 bit settings files exist.
 # on a 32-bit installation, only the settings32 exists.
 # -> so first check for the 64-bit settings file.
+#
 #       special case/path for Colossus Supercomputer
 if [ -e /usr/local/Xilinx/14.7/ISE_DS/settings64.sh ]; then
   echo "Detected 64-bit Xilinx installation on Colossus"
@@ -54,6 +69,8 @@ else
   echo "Cannot detect a Xilinx installation"
   exit 0;
 fi
+echo " "
+
 
 # time for the output filenames
 datetime2=`date +%m%d%H%M`
@@ -65,82 +82,100 @@ branch=`git status -b -s | head -n 1`
 branch2=${branch:3:6}
 
 
-outfile0="${LOGDIR}/compile-${datetime2}_0.log"
-outfile1="${LOGDIR}/compile-${datetime2}_1-xst.log"
-outfile2="${LOGDIR}/compile-${datetime2}_2-ngd.log"
-outfile3="${LOGDIR}/compile-${datetime2}_3-map.log"
-outfile4="${LOGDIR}/compile-${datetime2}_4-par.log"
-outfile5="${LOGDIR}/compile-${datetime2}_5-trc.log"
-outfile6="${LOGDIR}/compile-${datetime2}_6-bit.log"
+# timestamp the compile-logs
+outfile0="${BASEDIR}/${LOGDIR}/compile-${datetime2}_0.log"
+outfile1="${BASEDIR}/${LOGDIR}/compile-${datetime2}_1-xst.log"
+outfile2="${BASEDIR}/${LOGDIR}/compile-${datetime2}_2-ngd.log"
+outfile3="${BASEDIR}/${LOGDIR}/compile-${datetime2}_3-map.log"
+outfile4="${BASEDIR}/${LOGDIR}/compile-${datetime2}_4-par.log"
+outfile5="${BASEDIR}/${LOGDIR}/compile-${datetime2}_5-trc.log"
+outfile6="${BASEDIR}/${LOGDIR}/compile-${datetime2}_6-bit.log"
 
+
+# debug
+echo "Compiling for the Nexys4${TARGET_TAG} board" >> $outfile0
+
+
+# ISE build parameters
 ISE_COMMON_OPTS="-intstyle ise"
-ISE_NGDBUILD_OPTS="-p xc7a100t-csg324-1 -dd _ngo -sd ipcore_dir -nt timestamp"
+ISE_NGDBUILD_OPTS="-p xc7a100t-csg324-1 -dd _ngo -sd ../../../ipcore_dir -nt timestamp"
 ISE_MAP_OPTS="-p xc7a100t-csg324-1 -w -logic_opt on -ol high -t 1 -xt 0 -register_duplication on -r 4 -mt off -ir off -ignore_keep_hierarchy -pr b -lc off -power off"
 ISE_PAR_OPTS="-w -ol std -mt off"
 ISE_TRCE_OPTS="-v 3 -s 1 -n 3 -fastpaths -xml"
 
-# ensure these directory exists, if not, make them
-if test ! -e    "./isework/xst/"; then
-  echo "Creating ./isework/xst/"
-  mkdir          ./isework/xst/
-fi
-if test ! -e    "./isework/xst/projnav.tmp/"; then
-  echo "Creating ./isework/xst/projnav.tmp/"
-  mkdir          ./isework/xst/projnav.tmp
-fi
 
-# begin the ISE build:
-echo "Beginning the ISE build."
-echo " "
-echo "Check ./${LOGDIR}/compile-<datetime>-X.log for the log files, X={1,2,3,4,5,6}"
+# move into here so that all the ISE-generated output does not fill the base directory
+cd ${BASEDIR}/ise147pn/mega65-${TARGET_TAG}/working
+echo "Changing into:"
+pwd
 echo " "
 
-# first, put the git-commit-ID in the first log file.
+# create the nessessary ISE build-artifact directories
+if test ! -e    "${BASEDIR}/ise147pn/mega65-${TARGET_TAG}/working/xst/"; then
+  echo "Creating ${BASEDIR}/ise147pn/mega65-${TARGET_TAG}/working/xst/"
+  mkdir          ${BASEDIR}/ise147pn/mega65-${TARGET_TAG}/working/xst/
+fi
+if test ! -e    "${BASEDIR}/ise147pn/mega65-${TARGET_TAG}/working/xst/projnav.tmp/"; then
+  echo "Creating ${BASEDIR}/ise147pn/mega65-${TARGET_TAG}/working/xst/projnav.tmp/"
+  mkdir          ${BASEDIR}/ise147pn/mega65-${TARGET_TAG}/working/xst/projnav.tmp
+fi
+
+
+# debug, put the git-commit-ID in the first log file.
 echo ${gitstring} > $outfile0
 # put the git-branch-ID in the log file.
 echo ${branch}  >> $outfile0
 echo ${branch2} >> $outfile0
 
-NONDDR="nonddr" # filename to indicate target is Nexys4 (non-ddr) board
-# if the file exists, target the non-ddr board
-if test -e ${NONDDR}; then
-  echo "Compiling for the Nexys4 (non-DDR) board."
-  UCF_TARGET_FPGA="./src/vhdl/container-nonddr.ucf"
-  # format a tag to either be "_nonddr" or an empty-string, for the output filename BIT-file
-  NONDDR_TAG="_${NONDDR}"
-else
-  echo "Compiling for the Nexys4DDR board (the default)."
-  echo "To compile for the Nexys4 (non-DDR) board, the \"./${NONDDR}\" file must exist"
-  UCF_TARGET_FPGA="./src/vhdl/container-ddr.ucf"
-  NONDDR_TAG=""
-fi
-echo "Compiling for the Nexys4${NONDDR_TAG} board" >> $outfile0
 
+# convenient path to access the ISE build-artifacts
+SCROUTDIR_FULL="${BASEDIR}/ise147pn/mega65-${TARGET_TAG}/working/${SCROUTDIR}"
+#
+if test ! -e    "${SCROUTDIR_FULL}"; then
+  echo "Creating ${SCROUTDIR_FULL}"
+  mkdir          ${SCROUTDIR_FULL}
+fi
+
+
+# begin the ISE build:
+echo "Beginning the ISE build."
+echo "Check ./${LOGDIR}/compile-<datetime>-X.log for the log files, X={1,2,3,4,5,6}"
 echo " "
-cat ./src/version.a65
+
+cat ${BASEDIR}/src/version.a65
 pwd
 echo " "
+
 
 #
 # ISE: synthesize
 #
 datetime=`date +%Y%m%d_%H:%M:%S`
 echo "==> $datetime Starting: xst, see container.syr"
-xst ${ISE_COMMON_OPTS} -ifn "./isework/container.xst" -ofn "./isework/container.syr" >> $outfile1
+xst ${ISE_COMMON_OPTS} -ifn "container.xst" -ofn "${SCROUTDIR_FULL}/container.syr" >> $outfile1
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "xst failed with return code $retcode" && exit 1
+  echo "xst failed with return code $retcode" &&
+  cat $outfile1 | grep ERROR
+  exit 1
+else
+  cat $outfile1 | grep WARN
 fi
+
 
 #
 # ISE: ngdbuild
 #
 datetime=`date +%Y%m%d_%H:%M:%S`
 echo "==> $datetime Starting: ngdbuild, see container.bld"
-ngdbuild ${ISE_COMMON_OPTS} ${ISE_NGDBUILD_OPTS} -uc ${UCF_TARGET_FPGA} ./isework/container.ngc ./isework/container.ngd > $outfile2
+ngdbuild ${ISE_COMMON_OPTS} ${ISE_NGDBUILD_OPTS} -uc ${UCF_TARGET_FPGA} container.ngc ${SCROUTDIR_FULL}/container.ngd > $outfile2
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "ngdbuild failed with return code $retcode" && exit 1
+  echo "ngdbuild failed with return code $retcode" &&
+  cat $outfile2 | grep ERROR
+  exit 1
+else
+  cat $outfile2 | grep WARN
 fi
 
 #
@@ -148,10 +183,14 @@ fi
 #
 datetime=`date +%Y%m%d_%H:%M:%S`
 echo "==> $datetime Starting: map, see container_map.mrp"
-map ${ISE_COMMON_OPTS} ${ISE_MAP_OPTS} -o ./isework/container_map.ncd ./isework/container.ngd ./isework/container.pcf > $outfile3
+map ${ISE_COMMON_OPTS} ${ISE_MAP_OPTS} -o ${SCROUTDIR_FULL}/container_map.ncd ${SCROUTDIR_FULL}/container.ngd ${SCROUTDIR_FULL}/container.pcf > $outfile3
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "map failed with return code $retcode" && exit 1
+  echo "map failed with return code $retcode" &&
+  cat $outfile3 | grep ERROR
+  exit 1
+else
+  cat $outfile3 | grep WARN
 fi
 
 #
@@ -159,10 +198,14 @@ fi
 #
 datetime=`date +%Y%m%d_%H:%M:%S`
 echo "==> $datetime Starting: par, see container.par"
-par ${ISE_COMMON_OPTS} ${ISE_PAR_OPTS} ./isework/container_map.ncd ./isework/container.ncd ./isework/container.pcf > $outfile4
+par ${ISE_COMMON_OPTS} ${ISE_PAR_OPTS} ${SCROUTDIR_FULL}/container_map.ncd ${SCROUTDIR_FULL}/container.ncd ${SCROUTDIR_FULL}/container.pcf > $outfile4
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "par failed with return code $retcode" && exit 1
+  echo "par failed with return code $retcode" &&
+  cat $outfile4 | grep ERROR
+  exit 1
+else
+  cat $outfile4 | grep WARN
 fi
 
 #
@@ -170,10 +213,14 @@ fi
 #
 datetime=`date +%Y%m%d_%H:%M:%S`
 echo "==> $datetime Starting: trce, see container.twr"
-trce ${ISE_COMMON_OPTS} ${ISE_TRCE_OPTS} ./isework/container.twx ./isework/container.ncd -o ./isework/container.twr ./isework/container.pcf -ucf ./src/containerNULL.ucf > $outfile5
+trce ${ISE_COMMON_OPTS} ${ISE_TRCE_OPTS} ${SCROUTDIR_FULL}/container.twx ${SCROUTDIR_FULL}/container.ncd -o ${SCROUTDIR_FULL}/container.twr ${SCROUTDIR_FULL}/container.pcf > $outfile5
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "trce failed with return code $retcode" && exit 1
+  echo "trce failed with return code $retcode" &&
+  cat $outfile5 | grep ERROR
+  exit 1
+else
+  cat $outfile5 | grep WARN
 fi
 
 #
@@ -181,11 +228,16 @@ fi
 #
 datetime=`date +%Y%m%d_%H:%M:%S`
 echo "==> $datetime Starting: bitgen, see container.bgn"
-bitgen ${ISE_COMMON_OPTS} -f ./isework/container.ut ./isework/container.ncd > $outfile6
+bitgen ${ISE_COMMON_OPTS} -f container.ut ${SCROUTDIR_FULL}/container.ncd > $outfile6
 retcode=$?
 if [ $retcode -ne 0 ] ; then
-  echo "bitgen failed with return code $retcode" && exit 1
+  echo "bitgen failed with return code $retcode" &&
+  cat $outfile6 | grep ERROR
+  exit 1
+else
+  cat $outfile6 | grep WARN
 fi
+
 
 #
 # ISE -> all done
@@ -207,20 +259,25 @@ echo "From $outfile5: =================================================" >> $out
  tail -n 1 $outfile5 >> $outfile0
 echo "From $outfile6: =================================================" >> $outfile0
  echo "Nil" >> $outfile0
-
 echo " "
-# now prepare the sdcard-output directory by moving any existing bit-file
-for filename in ./sdcard-files/*.bit; do
-  echo "mv ${filename} ./sdcard-files/old-bitfiles"
-        mv ${filename} ./sdcard-files/old-bitfiles
-done
+
+
+## now prepare the sdcard-output directory by moving any existing bit-file
+if test -e                ${BASEDIR}/sdcard-files/*.bit; then
+  echo "Found OLD bit files, so will move them."
+  for filename in ${BASEDIR}/sdcard-files/*.bit; do
+    echo " mv ${filename} ${BASEDIR}/sdcard-files/old-bitfiles"
+           mv ${filename} ${BASEDIR}/sdcard-files/old-bitfiles
+  done
+else
+  echo "No existing bit files found."
+fi
+echo " "
+
 
 # now copy the bit-file to the sdcard-output directory, and timestamp it with time and git-status
-echo "cp ./isework/container.bit ./sdcard-files/bit${datetime2}_${branch2}_${gitstring}${NONDDR_TAG}.bit"
-cp       ./isework/container.bit ./sdcard-files/bit${datetime2}_${branch2}_${gitstring}${NONDDR_TAG}.bit
-# # and the KICKUP file
-# echo "cp ./src/KICKUP.M65 ./sdcard-files"
-#       cp ./src/KICKUP.M65 ./sdcard-files
-
+echo "cp ${SCROUTDIR_FULL}/container.bit ${BASEDIR}/sdcard-files/bit${datetime2}_${branch2}_${gitstring}_${TARGET_TAG}.bit"
+cp       ${SCROUTDIR_FULL}/container.bit ${BASEDIR}/sdcard-files/bit${datetime2}_${branch2}_${gitstring}_${TARGET_TAG}.bit
 echo " "
-ls -al ./sdcard-files
+
+ls -al ${BASEDIR}/sdcard-files
